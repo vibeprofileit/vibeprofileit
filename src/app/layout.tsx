927b5fa3a00f8084424f9f129fdd69dfa2f8dcb7
vibeprofileit <haktandalbu@gmail.com>
2026-05-13 00:02:18 +0300

    xx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -35,6 +35,10 @@ export default function RootLayout({
       style={{ colorScheme: "dark" }}
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
+      <head>
+        {/* eslint-disable-next-line @next/next/no-sync-scripts */}
+        <script dangerouslySetInnerHTML={{ __html: `(function(){var o=new MutationObserver(function(ms){ms.forEach(function(m){if(m.type!=='attributes')return;var n=m.attributeName;if(/^data-darkreader/.test(n)){m.target.removeAttribute(n);}else if(n==='style'){var s=m.target.style,ps=[];for(var i=0;i<s.length;i++)if(/--darkreader/.test(s[i]))ps.push(s[i]);ps.forEach(function(p){s.removeProperty(p);});}});});o.observe(document.documentElement,{attributes:true,subtree:true});})();` }} />
+      </head>
       <body className="min-h-full flex flex-col">
           <AuthWrapper>{children}</AuthWrapper>
           <FeedbackButton />
