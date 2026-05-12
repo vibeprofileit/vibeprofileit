a2c5fd358d637b1ea8ae6526d0d631cff8e6e7a2
vibeprofileit <haktandalbu@gmail.com>
2026-05-13 00:00:21 +0300

    xx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -32,6 +32,7 @@ export default function RootLayout({
       lang="en"
       suppressHydrationWarning
       data-scroll-behavior="smooth"
+      style={{ colorScheme: "dark" }}
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
