9279a2c9c166af58856396c6357acd290c7235b6
vibeprofileit <haktandalbu@gmail.com>
2026-05-13 00:04:31 +0300

    xx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 import AuthWrapper from "@/components/AuthWrapper"
 import FeedbackButton from "@/components/FeedbackButton";
@@ -35,11 +36,12 @@ export default function RootLayout({
       style={{ colorScheme: "dark" }}
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <head>
-        {/* eslint-disable-next-line @next/next/no-sync-scripts */}
-        <script dangerouslySetInnerHTML={{ __html: `(function(){var o=new MutationObserver(function(ms){ms.forEach(function(m){if(m.type!=='attributes')return;var n=m.attributeName;if(/^data-darkreader/.test(n)){m.target.removeAttribute(n);}else if(n==='style'){var s=m.target.style,ps=[];for(var i=0;i<s.length;i++)if(/--darkreader/.test(s[i]))ps.push(s[i]);ps.forEach(function(p){s.removeProperty(p);});}});});o.observe(document.documentElement,{attributes:true,subtree:true});})();` }} />
-      </head>
       <body className="min-h-full flex flex-col">
+        <Script
+          id="remove-darkreader"
+          strategy="beforeInteractive"
+          dangerouslySetInnerHTML={{ __html: `(function(){var o=new MutationObserver(function(ms){ms.forEach(function(m){if(m.type!=='attributes')return;var n=m.attributeName;if(/^data-darkreader/.test(n)){m.target.removeAttribute(n);}else if(n==='style'){var s=m.target.style,ps=[];for(var i=0;i<s.length;i++)if(/--darkreader/.test(s[i]))ps.push(s[i]);ps.forEach(function(p){s.removeProperty(p);});}});});o.observe(document.documentElement,{attributes:true,subtree:true});})();` }}
+        />
           <AuthWrapper>{children}</AuthWrapper>
           <FeedbackButton />
         </body>
